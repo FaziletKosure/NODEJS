6c64450391111664aa554a31e8283277fe6c8335
FaziletKosure <faziletkosure1@gmail.com>
2021-02-26 18:19:03 +0100

    added routing (html pages)

--- myApp/app.js
+++ myApp/app.js
@@ -4,9 +4,13 @@ const app = express()
 app.listen(8080)
 
 app.get('/',(req,res)=>{
-    res.send('<p>Home page</p>')
+    res.sendFile('./views/index.html',{root:__dirname})
 })
 
 app.get('/add-item',(req,res)=>{
-    res.send('<h2>Add Items</h2>')
+    res.sendFile('./views/add-item.html',{root:__dirname})
+})
+
+app.use((req,res)=>{
+    res.sendFile('./views/error.html',{root:__dirname})
 })
