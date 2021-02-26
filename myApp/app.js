3873a7b9c94e8acd57bcfb9059f638b8336f727e
FaziletKosure <faziletkosure1@gmail.com>
2021-02-26 20:50:10 +0100

    added ejs template engine

--- myApp/app.js
+++ myApp/app.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const app = express()
+app.set('view engine', 'ejs');
 
 app.listen(8080)
 
+// app.get('/',(req,res)=>{
+//     res.sendFile('./views/index.html',{root:__dirname})
+// })
+
 app.get('/',(req,res)=>{
-    res.sendFile('./views/index.html',{root:__dirname})
+    res.render('index')
 })
+
+// app.get('/add-item',(req,res)=>{
+//     res.sendFile('./views/add-item.html',{root:__dirname})
+// })
 
 app.get('/add-item',(req,res)=>{
-    res.sendFile('./views/add-item.html',{root:__dirname})
+    res.render('add-item')
 })
 
+// app.use((req,res)=>{
+//     res.sendFile('./views/error.html',{root:__dirname})
+// })
 app.use((req,res)=>{
-    res.sendFile('./views/error.html',{root:__dirname})
+    res.render('error')
 })
